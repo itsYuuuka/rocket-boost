c2fab9368ad18cc2beb5f7a1aa51828593810229
itsYuuuka <okan_altun@hotmail.com>
2025-08-27 22:27:08 +0200

    Implement thrust functionality

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
+    [SerializeField] InputAction thrust;
+    [SerializeField] float thrustForce = 100f;
+
+    Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        thrust.Enable();
+    }
+
+    private void FixedUpdate()
+    {
+        if (thrust.IsPressed())
+        {
+            rb.AddRelativeForce(Vector3.up * thrustForce * Time.fixedDeltaTime);
+        }
     }
 }
