223f2743e76c084a8e9d51e643224dd6103b7746
itsYuuuka <okan_altun@hotmail.com>
2025-08-28 23:32:40 +0200

    Add CollisionHandler script for game object interactions

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                Debug.Log("You finished!");
+                break;
+            case "Fuel":
+                Debug.Log("You found fuel!");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+
+        void ReloadLevel()
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex);
+        }
+    }
+}
